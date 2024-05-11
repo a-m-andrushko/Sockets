2316c54058fae6916cc9edf7d871639974538d10
Mako-95 <136629443+Mako-95@users.noreply.github.com>
2024-05-11 13:59:50 +0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -6,67 +6,71 @@
 
 int main()
 {
-    // UTWORZENIE GNIAZDA
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1)
-    {
-        std::cerr << "Error: Nie udało się utworzyć gniazda!\n";
-        exit(0);
-    }
+	// UTWORZENIE GNIAZDA
+	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == -1)
+	{
+		std::cerr << "Error: Nie udało się utworzyć gniazda!\n";
+		exit(0);
+	}
 
-    // BINDING GNIAZDA DO PORTU IP SERWERA
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8880);   // JAK W SERWERZE
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    if (connect(client_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1)
-    {
-        std::cerr << "Error: Nie udało się nawiązać połączenia z serwerem!\n";
-        exit(0);
-    }
+	// BINDING GNIAZDA DO PORTU IP SERWERA
+	sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(8880);   // JAK W SERWERZE
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	if (connect(client_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1)
+	{
+		std::cerr << "Error: Nie udało się nawiązać połączenia z serwerem!\n";
+		exit(0);
+	}
 
-    // KOMUNIKACJA
-    char buffer[4096];
-    while (true)
-    {
-        // NADAWANIE DO SERWERA
-        std::string user_input;
-        std::cout << "> ";
-        std::getline(std::cin, user_input);
-        if (user_input == "exit_client")
-        {
-            std::cout << "Klient kończy połączenie!" << "\n";
-            send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
-            close(client_socket);
-            break;
-        }
+	// KOMUNIKACJA
+	char buffer[4096];
+	while (true)
+	{
+		// NADAWANIE DO SERWERA
+		std::string user_input;
+		std::cout << "> ";
+		std::getline(std::cin, user_input);
+		if (user_input == "exit_client")
+		{
+			std::cout << "Klient kończy połączenie!" << "\n";
+			send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
+			close(client_socket);
+			break;
+		}
 
-        int send_result = send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
-        if (send_result == -1)
-        {
-            std::cerr << "Error: Błąd podczas nadawania!\n";
-            break;
-        }
-        
+		int send_result = send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
+		if (send_result == -1)
+		{
+			std::cerr << "Error: Błąd podczas nadawania!\n";
+			break;
+		}
 
-        // ODBIERANIE OD SERWERA
-        memset(buffer, 0, 4096);
-        int bytes_received = recv(client_socket, buffer, 4096, 0);
-        if (bytes_received == -1)
-        {
-            std::cerr << "Error: Błąd podczas odbierania!\n";
-            break;
-        }
-        std::cout << "Server: " << std::string(buffer, bytes_received) << "\n";
+		// ODBIERANIE OD SERWERA
+		memset(buffer, '\0', 4096);
+		int bytes_received = recv(client_socket, buffer, 4096, 0);
+		if (bytes_received == -1)
+		{
+			std::cerr << "Error: Błąd podczas odbierania!\n";
+			break;
+		}
+		std::cout << "Server: " << std::string(buffer, bytes_received) << "\n";
 
-        // SPRAWDZENIE, CZY SERWER SIĘ ROZŁĄCZA
-        if (std::string(buffer) == "exit_server")
-        {
-            std::cout << "Serwer się rozłączył!\n";
-            close(client_socket);
-            break;
-        }
-    }
+		if (std::string(buffer, bytes_received) == "exit_server")
+		{
+			close(client_socket);
+		}
+	}
 
-    return 0;
+	// ZAMKNIĘCIE GNIAZDA KLIENTA
+	if (client_socket != -1)
+	{
+		std::cout << "Serwer się rozłączył!\n";
+		close(client_socket);
+		break;
+	}
+
+	return 0;
 }
