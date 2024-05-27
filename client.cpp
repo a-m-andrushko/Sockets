b9cee4c973966c20790123228e36aafc25ed0a8d
Mako-95 <136629443+Mako-95@users.noreply.github.com>
2024-05-27 07:29:30 +0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,8 +3,13 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-int main()
+int turn = 0;
+
+int main(int arc, char *argv[])
 {
 	// UTWORZENIE GNIAZDA
 	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -16,8 +21,13 @@ int main()
 
 	// BINDING GNIAZDA DO PORTU IP SERWERA
 	sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8880);   // JAK W SERWERZE
+	//server_address.sin_family = AF_INET;
+	
+	struct hostent *h;
+	h = gethostbyname(argv[1]);
+	server_address.sin_family = h->h_addrtype;
+	
+	server_address.sin_port = htons(8080);   // JAK W SERWERZE
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	if (connect(client_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1)
 	{
@@ -29,45 +39,55 @@ int main()
 	char buffer[4096];
 	while (true)
 	{
-		// NADAWANIE DO SERWERA
-		std::string user_input;
-		std::cout << "> ";
-		std::getline(std::cin, user_input);
-		if (user_input == "exit_client")
+		if(turn == 0)
 		{
-			std::cout << "Klient kończy połączenie!" << "\n";
-			send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
-			close(client_socket);
-			break;
+			// NADAWANIE DO SERWERA
+			std::string user_input;
+			std::cout << "> ";
+			std::getline(std::cin, user_input);
+			if (user_input == "exit_client")
+			{
+				std::cout << "Klient kończy połączenie!" << "\n";
+				send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
+				close(client_socket);
+				break;
+			}
+			else if(user_input == "1" || user_input == "2" || user_input == "3" || user_input == "4" || user_input == "5" || user_input == "6" || user_input == "7" || user_input == "8" || user_input == "9")
+			{
+				int send_result = send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
+				if (send_result == -1)
+				{
+					std::cerr << "Error: Błąd podczas nadawania!\n";
+					break;
+				}
+				turn = 0;
+			}
+			else
+			{
+				std::cout << "Invalid input! Please enter a number from 1-9 range or exit_client to terminate game." << "\n";
+			}
 		}
-
-		int send_result = send(client_socket, user_input.c_str(), user_input.size() + 1, 0);
-		if (send_result == -1)
+		if(turn == 1)
 		{
-			std::cerr << "Error: Błąd podczas nadawania!\n";
-			break;
+			// ODBIERANIE OD SERWERA
+			memset(buffer, '\0', 4096);
+			int bytes_received = recv(client_socket, buffer, 4096, 0);
+			if (bytes_received == -1)
+			{
+				std::cerr << "Error: Błąd podczas odbierania!\n";
+				break;
+			}
+			std::cout << "Server: " << std::string(buffer, bytes_received) << "\n";
+	
+			// SPRAWDZENIE, CZY SERWER SIĘ ROZŁĄCZA
+			if (std::string(buffer) == "exit_server")
+			{
+				std::cout << "Serwer się rozłączył!\n";
+				close(client_socket);
+				break;
+			}
+			turn = 0;
 		}
-
-		// ODBIERANIE OD SERWERA
-		memset(buffer, '\0', 4096);
-		int bytes_received = recv(client_socket, buffer, 4096, 0);
-		if (bytes_received == -1)
-		{
-			std::cerr << "Error: Błąd podczas odbierania!\n";
-			break;
-		}
-		std::cout << "Server: " << std::string(buffer, bytes_received) << "\n";
-
-		if (std::string(buffer, bytes_received) == "exit_server")
-		{
-			close(client_socket);
-		}
-	}
-
-	// ZAMKNIĘCIE GNIAZDA KLIENTA
-	if (client_socket != -1)
-	{
-		close(client_socket);
 	}
 
 	return 0;
