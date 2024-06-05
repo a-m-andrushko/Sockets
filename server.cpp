6822c211458bf1413e7172a8917b438736785020
Mako-95 <136629443+Mako-95@users.noreply.github.com>
2024-06-05 15:39:46 +0200

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -18,19 +18,25 @@ std::string dataRead(int data) // ZAMIANA int NA string
 	}
 }
 
-void printGrid() // WYŚWIETLENIE GRY
+std::string gridRead() // ZAMIANA int[][] NA string
 {
 	std::string sGrid = "\n "+dataRead(grid[0][0])+"|"+dataRead(grid[1][0])+"|"+dataRead(grid[2][0])+" \n — — — \n "+dataRead(grid[0][1])+"|"+dataRead(grid[1][1])+"|"+dataRead(grid[2][1])+" \n — — — \n "+dataRead(grid[0][2])+"|"+dataRead(grid[1][2])+"|"+dataRead(grid[2][2])+" \n";
+	return sGrid;
+}
+
+void printGrid() // WYŚWIETLENIE GRY
+{
+	std::string sGrid = gridRead();
 	std::cout << sGrid;
 }
 
 void sendGrid(int client_socket) // PRZESŁANIE GRY
 {
-	std::string sGrid = "\n "+dataRead(grid[0][0])+"|"+dataRead(grid[1][0])+"|"+dataRead(grid[2][0])+" \n — — — \n "+dataRead(grid[0][1])+"|"+dataRead(grid[1][1])+"|"+dataRead(grid[2][1])+" \n — — — \n "+dataRead(grid[0][2])+"|"+dataRead(grid[1][2])+"|"+dataRead(grid[2][2])+" \n";
+	std::string sGrid = gridRead();
 	send(client_socket, sGrid.c_str(), sGrid.size() + 1, 0);
 }
 
-void clearGrid()
+void clearGrid() // RESET GRY
 {
 	for(int i = 0; i < 3; i++)
 	{
@@ -41,7 +47,41 @@ void clearGrid()
 	}
 }
 
-void checkEnd()
+void checkOverlap(int tile, int client_socket)
+{
+	if(turn == 0)
+	{
+		switch(tile)
+		{
+			case 1: {if(grid[0][0] == 0){grid[0][0] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 2: {if(grid[1][0] == 0){grid[1][0] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 3: {if(grid[2][0] == 0){grid[2][0] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 4: {if(grid[0][1] == 0){grid[0][1] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 5: {if(grid[1][1] == 0){grid[1][1] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 6: {if(grid[2][1] == 0){grid[2][1] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 7: {if(grid[0][2] == 0){grid[0][2] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 8: {if(grid[1][2] == 0){grid[1][2] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+			case 9: {if(grid[2][2] == 0){grid[2][2] = -1; printGrid(); gameState = 0; break;} else {gameState = 2; break;}}
+		}
+	}
+	else if(turn == 1)
+	{
+		switch(tile)
+		{
+			case 1: {if(grid[0][0] == 0){grid[0][0] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 2: {if(grid[1][0] == 0){grid[1][0] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 3: {if(grid[2][0] == 0){grid[2][0] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 4: {if(grid[0][1] == 0){grid[0][1] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 5: {if(grid[1][1] == 0){grid[1][1] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 6: {if(grid[2][1] == 0){grid[2][1] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 7: {if(grid[0][2] == 0){grid[0][2] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 8: {if(grid[1][2] == 0){grid[1][2] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+			case 9: {if(grid[2][2] == 0){grid[2][2] = 1; sendGrid(client_socket); gameState = 0; break;} else {gameState = 2; break;}}
+		}
+	}
+}
+
+void checkEnd() // SPRAWDZENIE WYGRANEJ GRY
 {
 	int tmp = 0;
 	for(int i = 0; i < 3; i++)
@@ -64,6 +104,19 @@ void checkEnd()
 			tmp = grid[1][1];
 		}
 	}
+	if(tmp == 0)
+	{
+		int full = 0;
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				if(grid[i][j] == 0) {break;}
+				else {full++;}
+			}
+		}
+		if(full == 9) {gameState = -2; return;}
+	}
 	
 	gameState = tmp;
 }
@@ -74,7 +127,7 @@ int main(int argc, char *argv[])
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);   // socket(x, SOCK_STREAM, x) -- SYGNALIZUJE TCP
 	if (server_socket == -1)
 	{
-		std::cerr << "Error: Nie udało się utworzyć gniazda!\n";
+		std::cerr << "Error: Socket could not be created!\n";
 		exit(0);
 	}
 
@@ -82,132 +135,166 @@ int main(int argc, char *argv[])
 	sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_address.sin_port = htons(8080);   // DOWOLNY SWOBODNY PORT	
+	server_address.sin_port = htons(8008);   // DOWOLNY SWOBODNY PORT	
 	if (bind(server_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1)
 	{
-		std::cerr << "Error: Nie udało się powiązać gniazda do portu IP!\n";
+		std::cerr << "Error: Socket could not be binded to IP port!\n";
 		exit(0);
 	}
 
 	// NASŁUCHIWANIE
-	if (listen(server_socket, 5) == -1)
+	if (listen(server_socket, 1) == -1)
 	{
-		std::cerr << "Error: Nasłuchiwanie nie powiodło się!\n";
+		std::cerr << "Error: Listening failed!\n";
 		exit(0);
 	}
 
-	// POŁĄCZENIE
-	sockaddr_in client_address;
-	socklen_t client_size = sizeof(client_address);
-	int client_socket = accept(server_socket, (sockaddr*)&client_address, &client_size);
-	if (client_socket == -1)
-	{
-		std::cerr << "Error: Połączenie nie zostało nawiązane!\n";
-		exit(0);
-	}
-
-	// KOMUNIKACJA
-	char buffer[4096];
+	int client_socket;
 	while (true)
 	{
-		if(turn == 0)
-		{
-			// ODBIERANIE OD KLIENTA
-			memset(buffer, '\0', 4096);
-			int bytes_received = recv(client_socket, buffer, 4096, 0);
-			if (bytes_received == -1) // SPRAWDZENIE, CZY WIADOMOŚĆ OD KLIENTA ZOSTAŁA ODEBRANA
+		// POŁĄCZENIE
+		sockaddr_in client_address;
+		socklen_t client_size = sizeof(client_address);
+		client_socket = accept(server_socket, (sockaddr*)&client_address, &client_size);
+	
+		if (client_socket != -1)
+		{	
+			// KOMUNIKACJA
+			char buffer[4096];
+			while (true)
 			{
-				std::cerr << "Error: Błąd podczas odbierania!\n";
-				break;
-			}
-			else if (bytes_received == 0) // SPRAWDZENIE, CZY KLIENT NADAJE ODPOWIEDŹ
-			{
-				std::cout << "Klient został odłączony!\n";
-				break;
-			}
-			else if (std::string(buffer) == "exit") // SPRAWDZENIE, CZY KLIENT SIĘ ROZŁĄCZA
-			{
-				std::cout << "Klient się rozłączył!\n";
-				close(client_socket);
-				break;
-			}
-			else
-			{
-				std::cout << "Client: " << std::string(buffer, bytes_received) << "\n";
-				if(std::string(buffer) == "1"){grid[0][0] = -1; printGrid();}
-				else if(std::string(buffer) == "2"){grid[1][0] = -1; printGrid();}
-				else if(std::string(buffer) == "3"){grid[2][0] = -1; printGrid();}
-				else if(std::string(buffer) == "4"){grid[0][1] = -1; printGrid();}
-				else if(std::string(buffer) == "5"){grid[1][1] = -1; printGrid();}
-				else if(std::string(buffer) == "6"){grid[2][1] = -1; printGrid();}
-				else if(std::string(buffer) == "7"){grid[0][2] = -1; printGrid();}
-				else if(std::string(buffer) == "8"){grid[1][2] = -1; printGrid();}
-				else if(std::string(buffer) == "9"){grid[2][2] = -1; printGrid();}
-				
-				checkEnd();
-				if(gameState == -1)
+				if(turn == 0)
 				{
-					std::cout<<"You lost! The game will now quit."<<"\n";
-					std::string msg = "You won! The game will now quit.";
-					send(client_socket, msg.c_str(), msg.size() + 1, 0);
-					clearGrid();
-					gameState = 0;
+					// ODBIERANIE OD KLIENTA
+					memset(buffer, '\0', 4096);
+					int bytes_received = recv(client_socket, buffer, 4096, 0);
+					if (bytes_received == -1) // SPRAWDZENIE, CZY WIADOMOŚĆ OD KLIENTA ZOSTAŁA ODEBRANA
+					{
+						std::cerr << "Error: Receiving failure!\n";
+						break;
+					}
+					else if (bytes_received == 0) // SPRAWDZENIE, CZY KLIENT NADAJE ODPOWIEDŹ
+					{
+						std::cout << "No response from client!\n";
+						break;
+					}
+					else if (std::string(buffer) == "exit") // SPRAWDZENIE, CZY KLIENT SIĘ ROZŁĄCZA
+					{
+						std::cout << "Client disconnected!\n";
+						close(client_socket);
+						break;
+					}
+					else
+					{
+						std::cout << "Client: " << std::string(buffer, bytes_received) << "\n";
+						if(std::string(buffer) == "1"){checkOverlap(1, client_socket);}
+						else if(std::string(buffer) == "2"){checkOverlap(2, client_socket);}
+						else if(std::string(buffer) == "3"){checkOverlap(3, client_socket);}
+						else if(std::string(buffer) == "4"){checkOverlap(4, client_socket);}
+						else if(std::string(buffer) == "5"){checkOverlap(5, client_socket);}
+						else if(std::string(buffer) == "6"){checkOverlap(6, client_socket);}
+						else if(std::string(buffer) == "7"){checkOverlap(7, client_socket);}
+						else if(std::string(buffer) == "8"){checkOverlap(8, client_socket);}
+						else if(std::string(buffer) == "9"){checkOverlap(9, client_socket);}
+						
+						if(gameState == 2)
+						{
+							std::string msg = "Invalid move! Please choose an empty tile."+gridRead();
+							send(client_socket, msg.c_str(), msg.size() + 1, 0);
+						}
+						else
+						{
+							checkEnd();
+							if(gameState == -1)
+							{
+								std::cout << "You lost! The game will now quit.\n";
+								std::string msg = "You won! The game will now quit.";
+								send(client_socket, msg.c_str(), msg.size() + 1, 0);
+								clearGrid();
+								gameState = 0;
+								break;
+							}
+							else if(gameState == -2)
+							{
+								std::cout << "It's a tie! The game will now quit.\n";
+								std::string msg = "It's a tie! The game will now quit.\n";
+								send(client_socket, msg.c_str(), msg.size() + 1, 0);
+								clearGrid();
+								gameState = 0;
+								break;
+							}
+							turn = 1;
+						}
+					}
 				}
-				
-				turn = 1;
-			}
-		}
-		if(turn == 1)
-		{
-			// NADAWANIE DO KLIENTA
-			std::string user_input;
-			std::cout << "> ";
-			std::getline(std::cin, user_input);
-			if(user_input == "1" || user_input == "2" || user_input == "3" || user_input == "4" || user_input == "5" || user_input == "6" || user_input == "7" || user_input == "8" || user_input == "9")
-			{
-				int send_result;
-				if(user_input == "1"){grid[0][0] = 1; sendGrid(client_socket);}
-				else if(user_input == "2"){grid[1][0] = 1;}
-				else if(user_input == "3"){grid[2][0] = 1;}
-				else if(user_input == "4"){grid[0][1] = 1;}
-				else if(user_input == "5"){grid[1][1] = 1;}
-				else if(user_input == "6"){grid[2][1] = 1;}
-				else if(user_input == "7"){grid[0][2] = 1;}
-				else if(user_input == "8"){grid[1][2] = 1;}
-				else if(user_input == "9"){grid[2][2] = 1;}
-				else {send_result = send(client_socket, user_input.c_str(), user_input.size() + 1, 0);}
-				
-				if (send_result == -1)
-				{
-					std::cerr << "Error: Błąd podczas nadawania!\n";
-					break;
-				}
-				
-				checkEnd();
-				if(gameState == 1)
-				{
-					std::cout<<"You won! The game will now reset."<<"\n";
-					std::string msg = "You lost! The game will now quit.";
-					send(client_socket, msg.c_str(), msg.size() + 1, 0);
-					clearGrid();
-					gameState = 0;
-				}
-				else{sendGrid(client_socket);}
-				
-				turn = 0;
-			}
-			else
-			{
-				std::cout << "Invalid input! Please enter a number from 1-9 range or exit to terminate game." << "\n";
-			}
-		}
-	}
 
+				if(turn == 1)
+				{
+					// NADAWANIE DO KLIENTA
+					std::string user_input;
+					std::cout << "> ";
+					std::getline(std::cin, user_input);
+					if(user_input == "1" || user_input == "2" || user_input == "3" || user_input == "4" || user_input == "5" || user_input == "6" || user_input == "7" || user_input == "8" || user_input == "9")
+					{
+						int send_result;
+						if(user_input == "1"){checkOverlap(1, client_socket);}
+						else if(user_input == "2"){checkOverlap(2, client_socket);}
+						else if(user_input == "3"){checkOverlap(3, client_socket);}
+						else if(user_input == "4"){checkOverlap(4, client_socket);}
+						else if(user_input == "5"){checkOverlap(5, client_socket);}
+						else if(user_input == "6"){checkOverlap(6, client_socket);}
+						else if(user_input == "7"){checkOverlap(7, client_socket);}
+						else if(user_input == "8"){checkOverlap(8, client_socket);}
+						else if(user_input == "9"){checkOverlap(9, client_socket);}
+						
+						if(gameState == 2)
+						{
+							std::cout<<"Invalid move! Please choose an empty tile.";
+							printGrid();
+						}
+						else
+						{
+							if (send_result == -1)
+							{
+								std::cerr << "Error: Sending failure!\n";
+								break;
+							}
+							checkEnd();
+							if(gameState == 1)
+							{
+								std::cout<< "You won! The game will now quit.\n";
+								std::string msg = "You lost! The game will now quit.";
+								send(client_socket, msg.c_str(), msg.size() + 1, 0);
+								clearGrid();
+								gameState = 0;
+								break;
+							}
+							else if(gameState == -2)
+							{
+								std::cout << "It's a tie! The game will now quit.\n";
+								std::string msg = "It's a tie! The game will now quit.";
+								send(client_socket, msg.c_str(), msg.size() + 1, 0);
+								clearGrid();
+								gameState = 0;
+								break;
+							}
+							turn = 0;
+						}
+					}
+					else
+					{
+						std::cout << "Invalid input! Please, enter a number from 1-9 or exit to terminate the game.\n";
+					}
+				}
+			}
+		}
+	}	
+	
 	// ZAMKNIĘCIE GNIAZDA KLIENTA
 	if (client_socket != -1)
 	{
 		close(client_socket);
 	}
-
+	
 	return 0;
 }
